76c5f765c6ec19911d5ceb6df8563dde05832c1f
Nikhil Saraf <nsaraf@purestorage.com>
2021-11-05 14:21:55 -0700

    DONT PUSH TO UPSTREAM remove extra files for devinci-os

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import Desktop from './components/Desktop/Desktop.svelte';
-import './css/global.scss';
-
-const desktop = new Desktop({
-  target: document.getElementById('root'),
-});
-
-export default desktop;
